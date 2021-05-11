a2b59500f0e9cada42dbd74f8d4fc475b6cf4418
Daniel Kudies <daniel.kudies@googlemail.com>
2021-05-11 21:00:59 +0200

    changes

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <PublicHeadBar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PublicHeadBar from '@/components/Public/PublicHeadBar.vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: { PublicHeadBar },
+
+  data: () => ({
+  })
+})
+</script>
---
+++ src/components/Public/PublicHeadBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-app-bar app color="#6A76AB" dense dark shrink-on-scroll src="@/assets/PublicHeadBarImage.jpg" fade-img-on-scroll>
+      <template v-slot:img="{ props }">
+        <v-img v-bind="props" gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)" />
+      </template>
+
+      <v-toolbar-title>Cocktail-Book</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab to="/home">Home</v-tab>
+          <v-tab to="/about">Cocktails</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
